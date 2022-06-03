ecabff60210eca06dc612f41a7dd112b5110d7f6
Diego Huamani <dhuamanilu@unsa.edu.pe>
2022-06-02 21:56:28 -0500

    creando la clase estudiante en estudiante/models.py

--- estudiantes/models.py
+++ estudiantes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Estudiante(models.Model):
+	nombres=models.CharField(max_length=40)
+	apePaterno=models.CharField(max_length=40)
+	apeMaterno=models.CharField(max_length=40)
+	nacimiento=models.DateField()
+	dni=models.CharField(max_length=8,primary_key=True)
+	cui=models.CharField(max_length=8)
