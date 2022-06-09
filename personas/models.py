4e4740358384efe7b6ee80ed86a965718daa7863
Diego Huamani <dhuamanilu@unsa.edu.pe>
2022-06-09 18:40:21 -0500

    agragando un boolean field al modelo de personas

--- personas/models.py
+++ personas/models.py
@@ -5,6 +5,7 @@ class Persona(models.Model):
 	nombres=models.CharField(max_length=100)
 	apellidos=models.CharField(max_length=100)
 	edad=models.IntegerField()
+	donador=models.BooleanField()
 
 
 
