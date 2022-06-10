249bf49d8e244b82f4fb3d0b9cccff204db63ab6
Diego Huamani <dhuamanilu@unsa.edu.pe>
2022-06-09 19:39:52 -0500

    agregando la ruta de templates en variable TEMPLATES de settings.py ,ademas para que funcione en cualquier SO se utiliza os.path.join en vez de hacerlo directamento con / o \ ,por ultim o en myHomeView le decimos que retorne la renderizacion de home.html

--- inicio/views.py
+++ inicio/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 def myHomeView(request,*args, **kwargs):
 	print(args,kwargs)
 	print(request.user)
-	return HttpResponse('<h1> Hola mundo desde Django </h1>')
+	return render(request,"home.html",{})
 
 def anotherView(request):
 	return HttpResponse('<h1>Solo es otra página')
