d0ba94296ffa77116d70567fc7584540bb301a7d
Diego Huamani <dhuamanilu@unsa.edu.pe>
2022-06-09 21:56:23 -0500

    El tag for: se utilizo para recorrer una lista enviada desde el contexto

--- inicio/views.py
+++ inicio/views.py
@@ -7,12 +7,13 @@ def myHomeView(request,*args, **kwargs):
 	myContext = {
 	 	"myText": "Esto es sobre nosotros",
         "myNumber": 123,
+        "myList":[33,44,55],
     }
 	return render(request,"home.html",myContext)
 
 def anotherView(request):
 	return render(request,"home2.html",{})
-	
+
 def otraView(request, *args, **kwargs):
 	myContext = {
 	 	"myList": [12, 28, 78, 6,"cadena",False],
