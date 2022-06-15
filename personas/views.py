f60a3da9ec87582495dbad398e1d4ddd1deeeb6a
Diego Huamani <dhuamanilu@unsa.edu.pe>
2022-06-15 17:50:25 -0500

    Se envio el objeto entero en el contexto,para acceder a sus atributos se hace objeto.atributo

--- personas/views.py
+++ personas/views.py
@@ -4,8 +4,7 @@ from .models import Persona
 def personaTestView(request):
 	obj =Persona.objects.get(id=1)
 	context={
-		'nombre':obj.nombres,
-		'edad':obj.edad,
+		'objeto':obj,	
 	}
 	return render (request,'personas/test.html',context)
 
