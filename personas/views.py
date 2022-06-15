8b36e18a5f15c1a15dc0f18ccbf397f4f87b2380
Diego Huamani <dhuamanilu@unsa.edu.pe>
2022-06-15 18:22:16 -0500

    Se creo forms.py dentro de personas

--- personas/views.py
+++ personas/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Persona
 # Create your views here.
 def personaTestView(request):
-	obj =Persona.objects.get(id=3)
+	obj =Persona.objects.get(id=2)
 	context={
 		'objeto':obj,	
 	}
