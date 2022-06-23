1051014495685c866f2f63a9d258afe6c635a259
Diego Huamani <dhuamanilu@unsa.edu.pe>
2022-06-23 18:53:24 -0500

    Diapositiva 10 : probando los widgets

--- personas/forms.py
+++ personas/forms.py
@@ -13,7 +13,15 @@ class PersonaForm(forms.ModelForm):
 		]
 
 class RawPersonaForm(forms.Form):
-	nombres = forms.CharField(label = 'Your Name')
+	nombres = forms.CharField(
+		widget = forms.Textarea(
+			attrs ={'Placeholder': 'Solo tu nombre por favor',
+			'id':'nombreID',
+			'class':'special',
+			'cols':'10',
+			} 
+		)
+	)
 	apellidos= forms.CharField()
 	edad=forms.IntegerField(initial = 20)
 	donador=forms.BooleanField(label_suffix="====")
