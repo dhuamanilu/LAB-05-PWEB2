fbe1b411b16cfb206e7403b6113cc5738ebc935b
Diego Huamani <dhuamanilu@unsa.edu.pe>
2022-06-23 21:48:40 -0500

    Django 4 -> Diapositiva 18 :Redireccionando con redirect,cuando se borra a un objeto se redirecciona a su pagina anterior ,antes del delete/

--- personas/views.py
+++ personas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Persona
 from .forms import PersonaForm, RawPersonaForm
 # Create your views here.
@@ -62,5 +62,6 @@ def personasDeleteView(request, myID):
 	if request.method == 'POST':
 		print("lo borro")
 		obj.delete()
+		return redirect('../')
 	context={'objeto':obj}
 	return render(request,'personas/personasBorrar.html',context)
