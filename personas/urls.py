b2bd89b3e33883d72f12a13ee02ba321d04d1771
Diego Huamani <dhuamanilu@unsa.edu.pe>
2022-07-06 20:19:58 -0500

    DJANGO 5 ->Diapositiva 5 creando base.html  y persona_list.html

--- personas/urls.py
+++ personas/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from personas.views import (PersonaListView)
+from personas.views import (PersonaListView,personasDeleteView,personaCreateVista,
+                            personasShowObject)
 
 app_name='personas'
 urlpatterns=[
     path('',PersonaListView.as_view(),name='persona-list'),
+    path('add/',personaCreateVista,name='adding'),
+    path('<int:myID>/',personasShowObject,name='browsing'),
+    path('<int:myID>/delete/',personasDeleteView,name='deleting'),
 ]
