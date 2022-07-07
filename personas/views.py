76ae53c5706526705af1f4cc3a9db0290ca6d7e3
Diego Huamani <dhuamanilu@unsa.edu.pe>
2022-07-06 21:13:05 -0500

    DJANGO 5->Diapositiva  7 detail view,creando persona_detail.html , agregando el url de detail view en urlss.py ,en lass vista creando nueva vista basada en clase y cambiando get_Abosulte_url ya que el nombre del kwarg no es myID sino pk

--- personas/views.py
+++ personas/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Persona
 from .forms import PersonaForm, RawPersonaForm
-from django.views.generic.list import (ListView)
+from django.views.generic import (ListView,DetailView)
 # Create your views here.
 class PersonaListView(ListView):
 	model = Persona 
 	queryset=Persona.objects.filter(edad__lte='40')
 
-
-
+class PersonaDetailView(DetailView):
+	model = Persona
 
 
 
